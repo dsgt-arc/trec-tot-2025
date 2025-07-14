5ab5e256cf487ca19b0e32340f75a66570b63173
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2025-07-14 05:30:55 -0400

    Mine for various edge relationships

--- user/acmiyaguchi/02-network-eda/process.py
+++ user/acmiyaguchi/02-network-eda/process.py
@@ -1,6 +1,7 @@
 # /// script
 # dependencies = [
 #   "pyspark",
+#   "numpy",
 #   "luigi",
 #   "typer",
 # ]
@@ -14,10 +15,12 @@ from pathlib import Path
 app = typer.Typer()
 
 
-def get_spark(cores=16, memory="124g"):
+def get_spark(cores=24, memory="190g", shuffle_partitions=2000):
     spark = (
         SparkSession.builder.master(f"local[{cores}]")
         .config("spark.driver.memory", memory)
+        .config("spark.jars.packages", "graphframes:graphframes:0.8.4-spark3.5-s_2.13")
+        .config("spark.sql.shuffle.partitions", shuffle_partitions)
         .getOrCreate()
     )
     return spark
@@ -114,6 +117,122 @@ class GenerateEdgeList(luigi.Task):
         )
 
 
+class ArticleOneHop(luigi.Task):
+    """Do the same as GenerateEdgeList but use GraphFrames instead."""
+
+    page_path = luigi.Parameter()
+    links_path = luigi.Parameter()
+    output_path = luigi.Parameter()
+
+    def output(self):
+        return {
+            "edges": luigi.LocalTarget(
+                (Path(self.output_path) / "edges/_SUCCESS").as_posix()
+            ),
+            "nodes": luigi.LocalTarget(
+                (Path(self.output_path) / "nodes/_SUCCESS").as_posix()
+            ),
+        }
+
+    def load_graph(self):
+        spark = get_spark()
+        page = spark.read.parquet(self.page_path)
+        pagelinks = spark.read.parquet(self.links_path)
+
+        v = page.select(
+            F.col("page_id").alias("id"),
+            "page_namespace",
+            (F.col("page_is_redirect") != "0").alias("page_is_redirect"),
+        )
+        e = pagelinks.select(
+            F.col("pl_from").alias("src"), F.col("pl_target_id").alias("dst")
+        ).distinct()
+
+        return GraphFrame(v, e)
+
+    def write_graph(self, g):
+        g.edges.explain(extended=True)
+        g.edges.repartition(32).write.parquet(
+            (Path(self.output_path) / "edges").as_posix(), mode="overwrite"
+        )
+        g.vertices.explain(extended=True)
+        g.vertices.repartition(8).write.parquet(
+            (Path(self.output_path) / "nodes").as_posix(), mode="overwrite"
+        )
+
+    def run(self):
+        g = self.load_graph()
+        motif_edges = self.find_motif(g)
+
+        g_prime = GraphFrame(g.vertices, motif_edges).dropIsolatedVertices()
+        self.write_graph(g_prime)
+
+    def find_motif(self, g):
+        motif = (
+            g.find("(a)-[e]->(b)")
+            .where("a.id != b.id")
+            .where("a.page_namespace = 0 and a.page_is_redirect = false")
+            .where("b.page_namespace = 0 and b.page_is_redirect = false")
+        )
+        return motif.select("e.*", F.lit(1).alias("weight")).distinct()
+
+
+class ArticleMetaTwoHop(ArticleOneHop):
+    def find_motif(self, g):
+        motif = (
+            g.find("(a)-[]->(b); (b)-[]->(c)")
+            .where("a.id != c.id")
+            .where("a.page_namespace = 0 and a.page_is_redirect = false")
+            .where("b.page_namespace != 0 or b.page_is_redirect = true")
+            .where("c.page_namespace = 0 and c.page_is_redirect = false")
+        )
+        return (
+            motif.select(F.col("a.id").alias("src"), F.col("c.id").alias("dst"))
+            .groupBy("src", "dst")
+            .agg(F.count("*").alias("weight"))
+        )
+
+
+class ArticleSharedTarget(ArticleOneHop):
+    def find_motif(self, g):
+        motif = (
+            g.find("(a)-[]->(b); (c)-[]->(b)")
+            .where("a.id != c.id")
+            .where("a.page_namespace = 0 and a.page_is_redirect = false")
+            .where("c.page_namespace = 0 and c.page_is_redirect = false")
+        )
+        # we keep the page_namespace, in case we want to only look at
+        # two hops through articles, or two hops through meta.
+        # in this case we consider all two hops.
+        return (
+            motif.select(
+                F.col("a.id").alias("src"),
+                F.col("c.id").alias("dst"),
+                F.col("b.page_namespace").alias("page_namespace"),
+            )
+            .groupBy("src", "dst", "page_namespace")
+            .agg(F.count("*").alias("weight"))
+        )
+
+
+class ArticleCategorySharedTarget(ArticleSharedTarget):
+    def load_graph(self):
+        spark = get_spark()
+        page = spark.read.parquet(self.page_path)
+        categorylinks = spark.read.parquet(self.links_path)
+
+        v = page.select(
+            F.col("page_id").alias("id"),
+            "page_namespace",
+            (F.col("page_is_redirect") != "0").alias("page_is_redirect"),
+        )
+        e = categorylinks.select(
+            F.col("cl_from").alias("src"), F.col("cl_target_id").alias("dst")
+        ).distinct()
+
+        return GraphFrame(v, e)
+
+
 class Workflow(luigi.Task):
     def run(self):
         trec_root = Path("~/scratch/trec-tot-2025").expanduser()
@@ -127,12 +246,36 @@ class Workflow(luigi.Task):
         )
         yield id2wd_task
 
-        yield GenerateEdgeList(
-            mapping_path=(output_root / "id2wikidataid_6m/v1").as_posix(),
-            page_path=(parquet_root / "page").as_posix(),
-            pagelinks_path=(parquet_root / "pagelinks").as_posix(),
-            output_path=(output_root / "graph/v1").as_posix(),
-        )
+        yield [
+            GenerateEdgeList(
+                mapping_path=(output_root / "id2wikidataid_6m/v1").as_posix(),
+                page_path=(parquet_root / "page").as_posix(),
+                pagelinks_path=(parquet_root / "pagelinks").as_posix(),
+                output_path=(output_root / "graph/v1").as_posix(),
+            ),
+            ArticleOneHop(
+                page_path=(parquet_root / "page").as_posix(),
+                links_path=(parquet_root / "pagelinks").as_posix(),
+                output_path=(output_root / "graph/v2/article-one-hop").as_posix(),
+            ),
+            ArticleMetaTwoHop(
+                page_path=(parquet_root / "page").as_posix(),
+                links_path=(parquet_root / "pagelinks").as_posix(),
+                output_path=(output_root / "graph/v2/article-meta-two-hop").as_posix(),
+            ),
+            ArticleSharedTarget(
+                page_path=(parquet_root / "page").as_posix(),
+                links_path=(parquet_root / "pagelinks").as_posix(),
+                output_path=(output_root / "graph/v2/article-shared-target").as_posix(),
+            ),
+            ArticleCategorySharedTarget(
+                page_path=(parquet_root / "page").as_posix(),
+                links_path=(parquet_root / "categorylinks").as_posix(),
+                output_path=(
+                    output_root / "graph/v2/article-category-shared-target"
+                ).as_posix(),
+            ),
+        ]
 
 
 @app.command()
@@ -141,4 +284,7 @@ def run():
 
 
 if __name__ == "__main__":
+    spark = get_spark()
+    from graphframes import GraphFrame
+
     app()
